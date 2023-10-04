1d80be12428034a2131b79e7b887b6cfec7a325f
ashokkumarmala7 <ashokkumarmala7@gamail.com>
2023-10-04 06:42:39 +0000

    solution

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import GameResults from './components/GameResults'
+import './App.css'
+
+const choicesList = [
+  {
+    id: 'ROCK',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/rock-paper-scissor/rock-image.png',
+  },
+  {
+    id: 'SCISSORS',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/rock-paper-scissor/scissor-image.png',
+  },
+  {
+    id: 'PAPER',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/rock-paper-scissor/paper-image.png',
+  },
+]
+
+const App = () => <GameResults />
+
+export default App
---
+++ src/components/GameResults/styledComponents.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components'
+
+export const MainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
+  padding: 20px;
+  background-color: #223a5f;
+`
+export const RulesView = styled.div`
+  align-self: flex-end;
+  margin-top: auto;
+  @media screen and (max-width: 756px) {
+    margin-bottom: 40px;
+  }
+`
+export const PopUpView = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 40px;
+  justify-content: space-between;
+  padding: 10px;
+  @media screen and (max-width: 576px) {
+    height: 260px;
+    width: 100%;
+  }
+`
+export const PopUpImage = styled.img`
+  width: 100%;
+  height: 90%;
+  margin-top: auto;
+`
